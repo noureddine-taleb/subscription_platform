b39cea55d39968443c2e04fc77cef708c2242765
Nour-eddine Taleb <contact@noureddine.xyz>
2023-08-11 10:35:38 +0100

    add user resource

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -10,8 +11,9 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(WebsiteSeeder $website_seeder): void
+    public function run(WebsiteSeeder $website_seeder, UserSeeder $user_seeder): void
     {
         $website_seeder->run();
+        $user_seeder->run();
     }
 }
